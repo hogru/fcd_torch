72c9c3a32e33fe730d490b7dfc4f21d305579487
Stephan Holzgruber <3949272+hogru@users.noreply.github.com>
2022-08-26 14:33:26 +0200

    Backup before vacation, transformer in bad sstate

--- fcd_torch/utils.py
+++ fcd_torch/utils.py
@@ -1,90 +1,115 @@
 try:
     from rdkit import Chem
+
     _rdkit_available = True
 except ImportError as e:
     import warnings
-    warnings.warn("RDKit is not installed. Canonization won't work.\n"
-                  "Original Import Error below:\n" + str(e))
+
+    warnings.warn(
+        "RDKit is not installed. Canonization won't work.\n"
+        "Original Import Error below:\n" + str(e)
+    )
     _rdkit_available = False
 import numpy as np
-from contextlib import contextmanager
-import torch
 from torch import nn
 from torch.utils.data import Dataset
-from .torch_layers import Reverse, IndexTuple, \
-                          IndexTensor, Transpose, \
-                          SamePadding1d
+from .torch_layers import Reverse, IndexTuple, IndexTensor, Transpose, SamePadding1d
 from scipy import linalg
 
 __vocab = [
-    'C', 'N', 'O', 'H', 'F', 'Cl', 'P', 'B', 'Br',
-    'S', 'I', 'Si', '#', '(', ')', '+', '-', '1',
-    '2', '3', '4', '5', '6', '7', '8', '=', '[',
-    ']', '@', 'c', 'n', 'o', 's', 'X', '.'
+    "C",
+    "N",
+    "O",
+    "H",
+    "F",
+    "Cl",
+    "P",
+    "B",
+    "Br",
+    "S",
+    "I",
+    "Si",
+    "#",
+    "(",
+    ")",
+    "+",
+    "-",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "=",
+    "[",
+    "]",
+    "@",
+    "c",
+    "n",
+    "o",
+    "s",
+    "X",
+    ".",
 ]
 __vocab_i2c = {i: k for i, k in enumerate(__vocab)}
 __vocab_c2i = {k: i for i, k in enumerate(__vocab)}
-__unk = __vocab_c2i['X']
-__two_letters = {'r', 'i', 'l'}
+__unk = __vocab_c2i["X"]
+__two_letters = {"r", "i", "l"}
 
 
 def get_one_hot(smiles, pad_len=-1):
-    smiles = smiles + '.'
-    one_hot = np.zeros((
-        len(smiles) if pad_len < 0 else pad_len,
-        len(__vocab)
-    ))
+    smiles = smiles + "."
+    one_hot = np.zeros((len(smiles) if pad_len < 0 else pad_len, len(__vocab)))
     src = 0
     dst = 0
     while True:
         if smiles[src + 1] in __two_letters:
-            sym = smiles[src:src + 2]
+            sym = smiles[src : src + 2]
             src += 2
         else:
             sym = smiles[src]
             src += 1
         one_hot[dst, __vocab_c2i.get(sym, __unk)] = 1
         dst += 1
-        if smiles[src] == '.' or dst == one_hot.shape[0] - 1:
-            one_hot[dst, __vocab_c2i.get('.')] = 1
+        if smiles[src] == "." or dst == one_hot.shape[0] - 1:
+            one_hot[dst, __vocab_c2i.get(".")] = 1
             break
     return one_hot
 
 
 def load_imported_model(keras_config):
     activations = {
-        'relu': nn.ReLU,
-        'selu': nn.SELU,
-        'tanh': nn.Tanh,
-        'sigmoid': nn.Sigmoid,
+        "relu": nn.ReLU,
+        "selu": nn.SELU,
+        "tanh": nn.Tanh,
+        "sigmoid": nn.Sigmoid,
     }
     layers = []
     conv = True
     for layer_type, config in keras_config:
         state_dict, kwargs, other_info = config
-        if layer_type == 'Conv1d':
+        if layer_type == "Conv1d":
             assert conv, "Can't have conv layers after LSTM"
-            if other_info['padding'] == 'same':
-                layers.append(
-                    SamePadding1d(kwargs['kernel_size'],
-                                  kwargs['stride'])
-                )
+            if other_info["padding"] == "same":
+                layers.append(SamePadding1d(kwargs["kernel_size"], kwargs["stride"]))
             layer = nn.Conv1d(**kwargs)
             layer.load_state_dict(state_dict)
             layers.append(layer)
-            activation = other_info['activation']
+            activation = other_info["activation"]
             layers.append(activations[activation]())
-        elif layer_type == 'LSTM':
+        elif layer_type == "LSTM":
             if conv:
                 conv = False
                 layers.append(Transpose())
             layer = nn.LSTM(**kwargs)
             layer.load_state_dict(state_dict)
-            if other_info['reverse']:
+            if other_info["reverse"]:
                 layers.append(Reverse())
             layers.append(layer)
             layers.append(IndexTuple(0))
-            if other_info['last']:
+            if other_info["last"]:
                 layers.append(IndexTensor(-1, 1))
         else:
             raise ValueError("Unknown layer type")
@@ -96,7 +121,8 @@ class SmilesDataset(Dataset):
 
     def __init__(self, smiles_list, canonize=True):
         super().__init__()
-        self.smiles_list = smiles_list
+        # self.smiles_list = smiles_list
+        self.smiles_list = np.array(smiles_list)
         self.canonize = canonize
         if not _rdkit_available and canonize:
             raise ValueError("Can't canonize SMILES without installed RDKit")
@@ -108,7 +134,7 @@ class SmilesDataset(Dataset):
             if mol is None:
                 raise ValueError("Got invalid SMILES '{}'".format(smiles))
             smiles = Chem.MolToSmiles(mol)
-        features = get_one_hot(smiles, 350)
+        features = get_one_hot(smiles, self.__PAD_LEN)
         return features / features.shape[1]
 
     def __len__(self):
@@ -145,12 +171,12 @@ def calculate_frechet_distance(mu1, sigma1, mu2, sigma2, eps=1e-6):
     sigma1 = np.atleast_2d(sigma1)
     sigma2 = np.atleast_2d(sigma2)
 
-    assert mu1.shape == mu2.shape, (
-           "Training and test mean vectors have different lengths"
-    )
-    assert sigma1.shape == sigma2.shape, (
-           "Training and test covariances have different dimensions"
-    )
+    assert (
+        mu1.shape == mu2.shape
+    ), "Training and test mean vectors have different lengths"
+    assert (
+        sigma1.shape == sigma2.shape
+    ), "Training and test covariances have different dimensions"
 
     diff = mu1 - mu2
 
@@ -169,13 +195,14 @@ def calculate_frechet_distance(mu1, sigma1, mu2, sigma2, eps=1e-6):
 
     tr_covmean = np.trace(covmean)
 
-    return (diff.dot(diff) + np.trace(sigma1) +
-            np.trace(sigma2) - 2 * tr_covmean)
+    return diff.dot(diff) + np.trace(sigma1) + np.trace(sigma2) - 2 * tr_covmean
 
 
+"""
 @contextmanager
 def todevice(model, device):
     model.to(device)
     yield
-    model.to('cpu')
+    model.to("cpu")
     torch.cuda.empty_cache()
+"""
