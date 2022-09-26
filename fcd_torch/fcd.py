4dfcb0f0238a13b6f36a5c9c97b5bd385d0585b9
Stephan Holzgruber <3949272+hogru@users.noreply.github.com>
2022-09-26 11:20:07 +0200

    Experiment with memory problem/size

--- fcd_torch/fcd.py
+++ fcd_torch/fcd.py
@@ -1,12 +1,30 @@
-from fcd_torch.utils import SmilesDataset, \
-                            calculate_frechet_distance, \
-                            todevice, \
-                            load_imported_model
+from importlib import reload
+
+import fcd_torch.utils
+reload(fcd_torch.utils)
+
+from fcd_torch.utils import (
+    SmilesDataset,
+    calculate_frechet_distance,
+    # todevice,
+    load_imported_model,
+)
+import gc
 import torch
 from torch.utils.data import DataLoader
 import os
 import numpy as np
 import warnings
+
+from rich.progress import (
+    BarColumn,
+    MofNCompleteColumn,
+    Progress,
+    SpinnerColumn,
+    TaskProgressColumn,
+    TextColumn,
+    TimeRemainingColumn,
+)
 
 
 class FCD:
@@ -29,10 +47,16 @@ class FCD:
         pgen = fcd.precalc(smiles_list)
         fcd(smiles_list, pgen=pgen)
     """
-    def __init__(self, device='cpu', n_jobs=1,
-                 batch_size=512,
-                 model_path=None,
-                 canonize=True):
+
+    def __init__(
+        self,
+        device="cpu",
+        n_jobs=1,
+        batch_size=512,
+        model_path=None,
+        canonize=True,
+        pbar=False,
+    ):
         """
         Loads ChemNet on device
         params:
@@ -43,7 +67,7 @@ class FCD:
         """
         if model_path is None:
             model_dir = os.path.split(__file__)[0]
-            model_path = os.path.join(model_dir, 'ChemNet_v0.13_pretrained.pt')
+            model_path = os.path.join(model_dir, "ChemNet_v0.13_pretrained.pt")
 
         self.device = device
         self.n_jobs = n_jobs if n_jobs != 1 else 0
@@ -52,6 +76,7 @@ class FCD:
         self.model = load_imported_model(keras_config)
         self.model.eval()
         self.canonize = canonize
+        self.pbar = pbar
 
     def get_predictions(self, smiles_list):
         if len(smiles_list) == 0:
@@ -59,37 +84,72 @@ class FCD:
         dataloader = DataLoader(
             SmilesDataset(smiles_list, canonize=self.canonize),
             batch_size=self.batch_size,
-            num_workers=self.n_jobs
+            num_workers=self.n_jobs,
         )
-        with todevice(self.model, self.device), torch.no_grad():
+        with Progress(
+            SpinnerColumn(),
+            TextColumn("[progress.description]{task.description}"),
+            BarColumn(),
+            MofNCompleteColumn(),
+            TaskProgressColumn(),
+            TimeRemainingColumn(elapsed_when_finished=True),
+            refresh_per_second=2,
+            disable=not self.pbar,
+        ) as progress:
+
+            task = progress.add_task(
+                "Calculate activations...", total=len(dataloader)  # size=0
+            )
+
+            """
+            with todevice(self.model, self.device), torch.no_grad():
+                chemnet_activations = []
+                for batch in dataloader:
+                    chemnet_activations.append(
+                        self.model(
+                            batch.transpose(1, 2).float().to(self.device)
+                        ).to('cpu').detach().numpy()
+                    )
+            """
+
+            # This does not help
+            self.model.to(self.device)
+            self.model.eval()
             chemnet_activations = []
-            for batch in dataloader:
-                chemnet_activations.append(
-                    self.model(
-                        batch.transpose(1, 2).float().to(self.device)
-                    ).to('cpu').detach().numpy()
-                )
+            with torch.inference_mode():
+                for batch in dataloader:
+                    batch = batch.transpose(1, 2).float().to(self.device)
+                    activations = self.model(batch)
+                    activations = activations.to("cpu").detach().numpy()
+                    chemnet_activations.append(activations)
+                    progress.update(task, advance=1)
+                    gc.collect()
+                    # del batch  # this does not help
+                    # del activations
+
         return np.row_stack(chemnet_activations)
 
     def precalc(self, smiles_list):
         if len(smiles_list) < 2:
-            warnings.warn("Can't compute FCD for less than 2 molecules"
-                          "({} given)".format(len(smiles_list)))
+            warnings.warn(
+                "Can't compute FCD for less than 2 molecules"
+                "({} given)".format(len(smiles_list))
+            )
             return {}
         chemnet_activations = self.get_predictions(smiles_list)
         mu = chemnet_activations.mean(0)
         sigma = np.cov(chemnet_activations.T)
-        return {'mu': mu, 'sigma': sigma}
+        return {"mu": mu, "sigma": sigma}
 
     def metric(self, pref, pgen):
-        if 'mu' not in pref or 'sigma' not in pgen:
+        if "mu" not in pref or "sigma" not in pgen:
             warnings.warn("Failed to compute FCD (check ref)")
             return np.nan
-        if 'mu' not in pgen or 'sigma' not in pgen:
+        if "mu" not in pgen or "sigma" not in pgen:
             warnings.warn("Failed to compute FCD (check gen)")
             return np.nan
         return calculate_frechet_distance(
-            pref['mu'], pref['sigma'], pgen['mu'], pgen['sigma']
+            pref["mu"], pref["sigma"], pgen["mu"], pgen["sigma"]
         )
 
     def __call__(self, ref=None, gen=None, pref=None, pgen=None):
--- fcd_torch/keras2pytorch.py
+++ fcd_torch/keras2pytorch.py
@@ -38,7 +38,7 @@ def build_masked_loss(loss_function, mask_value):
 def load_keras_model(model_file):
     config = tf.ConfigProto(allow_soft_placement=True)
     config.gpu_options.allow_growth = True
-    with tf.device('/cpu'):
+    with tf.device("/cpu"):
         sess = tf.Session(config=config)
         set_session(sess)
         K.clear_session()
@@ -46,9 +46,9 @@ def load_keras_model(model_file):
         model = load_model(
             model_file,
             custom_objects={
-                'masked_loss_function': masked_loss_function,
-                'masked_accuracy': masked_accuracy
-            }
+                "masked_loss_function": masked_loss_function,
+                "masked_accuracy": masked_accuracy,
+            },
         )
         model.pop()
         model.pop()
@@ -57,28 +57,28 @@ def load_keras_model(model_file):
 
 def convert_LSTM_layer(keras_layer):
     pytorch_config = {
-        'input_size': keras_layer.input.shape[-1].value,
-        'hidden_size': keras_layer.output.shape[-1].value,
-        'batch_first': True
+        "input_size": keras_layer.input.shape[-1].value,
+        "hidden_size": keras_layer.output.shape[-1].value,
+        "batch_first": True,
     }
     pytorch_layer = nn.LSTM(**pytorch_config)
     W_ih, W_hh, b_ih = keras_layer.get_weights()
     pytorch_state = pytorch_layer.state_dict()
     for weight in pytorch_state:
-        if weight.startswith('weight_ih'):
+        if weight.startswith("weight_ih"):
             pytorch_state[weight] = torch.tensor(W_ih.T)
-        elif weight.startswith('weight_hh'):
+        elif weight.startswith("weight_hh"):
             pytorch_state[weight] = torch.tensor(W_hh.T)
-        elif weight.startswith('bias_hh'):
+        elif weight.startswith("bias_hh"):
             pytorch_state[weight] *= 0
-        elif weight.startswith('bias_ih'):
+        elif weight.startswith("bias_ih"):
             pytorch_state[weight] = torch.tensor(b_ih)
         else:
             raise ValueError("Unknown parameter {}".format(weight))
     pytorch_layer.load_state_dict(pytorch_state)
     other_info = {
-        'reverse': keras_layer.get_config()['go_backwards'],
-        'last': not keras_layer.get_config()['return_sequences']
+        "reverse": keras_layer.get_config()["go_backwards"],
+        "last": not keras_layer.get_config()["return_sequences"],
     }
     return pytorch_state, pytorch_config, other_info
 
@@ -86,13 +86,13 @@ def convert_LSTM_layer(keras_layer):
 def convert_Conv1d_layer(keras_layer):
     keras_config = keras_layer.get_config()
     pytorch_config = {
-        'in_channels': keras_layer.input.shape[-1].value,
-        'out_channels': keras_layer.output.shape[-1].value,
-        'kernel_size': keras_config['kernel_size'][0],
-        'stride': keras_config['strides'][0],
-        'padding': 0,
-        'dilation': keras_config['dilation_rate'],
-        'bias': keras_config['use_bias'],
+        "in_channels": keras_layer.input.shape[-1].value,
+        "out_channels": keras_layer.output.shape[-1].value,
+        "kernel_size": keras_config["kernel_size"][0],
+        "stride": keras_config["strides"][0],
+        "padding": 0,
+        "dilation": keras_config["dilation_rate"],
+        "bias": keras_config["use_bias"],
     }
     kernel = keras_layer.get_weights()
 
@@ -103,16 +103,16 @@ def convert_Conv1d_layer(keras_layer):
     else:
         kernel = kernel[0]
     for weight in pytorch_state:
-        if weight.startswith('weight'):
+        if weight.startswith("weight"):
             pytorch_state[weight] = torch.tensor(kernel.transpose(2, 1, 0))
-        elif weight.startswith('bias'):
+        elif weight.startswith("bias"):
             pytorch_state[weight] = torch.tensor(bias)
         else:
             raise ValueError("Unknown parameter {}".format(weight))
     pytorch_layer.load_state_dict(pytorch_state)
     other_info = {
-        'activation': keras_config['activation'],
-        'padding': keras_config['padding']
+        "activation": keras_config["activation"],
+        "padding": keras_config["padding"],
     }
     return pytorch_state, pytorch_config, other_info
 
@@ -121,26 +121,22 @@ def convert_keras2pytorch(keras_model):
     pytorch_config = []
     for layer in keras_model.layers:
         name = layer.name.lower()
-        if 'conv1d' in name:
-            pytorch_config.append(
-                ('Conv1d', convert_Conv1d_layer(layer))
-            )
-        elif 'lstm' in name:
-            pytorch_config.append(
-                ('LSTM', convert_LSTM_layer(layer))
-            )
+        if "conv1d" in name:
+            pytorch_config.append(("Conv1d", convert_Conv1d_layer(layer)))
+        elif "lstm" in name:
+            pytorch_config.append(("LSTM", convert_LSTM_layer(layer)))
     return pytorch_config
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--keras_model', type=str,
-                        help='Path to Keras model (.h5)')
-    parser.add_argument('--pytorch_model', type=str,
-                        help='Path to output Pytorch model (.pt)')
+    parser.add_argument("--keras_model", type=str, help="Path to Keras model (.h5)")
+    parser.add_argument(
+        "--pytorch_model", type=str, help="Path to output Pytorch model (.pt)"
+    )
     args, unknown = parser.parse_known_args(sys.argv[1:])
     if len(unknown) != 0:
-        raise ValueError('Unknown argument {}\n'.format(unknown[0]))
+        raise ValueError("Unknown argument {}\n".format(unknown[0]))
 
     keras_model = load_keras_model(args.keras_model)
     pytorch_config = convert_keras2pytorch(keras_model)
